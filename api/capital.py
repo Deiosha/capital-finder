0a1b350d5d5faa521f943545738a94f1ec0056cb
DEIOSHA SPARKS <DEIOSHASPARKS13@GMAIL.COM>
2023-05-10 15:21:25 -0700

    all functions working correctly

--- api/capital.py
+++ api/capital.py
@@ -1,4 +1,4 @@
-from http.server import BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib import parse
 import requests
 
@@ -13,15 +13,26 @@ class handler(BaseHTTPRequestHandler):
         # creating a dict object, contents are taken from query_string_list
         dictionary = dict(query_string_list)
 
+        if 'country' in dictionary:
+            country = dictionary['country']
+            dictionary_api_url = f"https://restcountries.com/v3.1/name/{country}"
+            response = requests.get(dictionary_api_url)
+            data = response.json()
+            capital = data[0]['capital'][0]
+            message = f"The capital of {country} is {capital}"
+        elif 'capital' in dictionary:
+            capital = dictionary['capital']
+            dictionary_api_url = f"https://restcountries.com/v3.1/capital/{capital}"
+            response = requests.get(dictionary_api_url)
+            data = response.json()
+            country = data[0]['name']['common']
+            message = f"{capital} is the capital of {country}"
+        else:
+            message = "Please enter a country and capital. Thank you!"
 
-# creating response
-        message = "Working"
-#       passing the code
         self.send_response(200)
-#       content type
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        # to write the message
         self.wfile.write(message.encode())
 
 if __name__ == '__main__':
